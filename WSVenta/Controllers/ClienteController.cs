4436ea899fbc1ac1c08bd1cce874ccc9ef05d2f4
Juan Martin Alfano <juanmartinalfano@gmail.com>
2020-09-25 21:35:58 -0300

    CRUD Cliente

--- WSVenta/Controllers/ClienteController.cs
+++ WSVenta/Controllers/ClienteController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using WSVenta.Models;
+using WSVenta.Models.Response;
+using WSVenta.Models.Request;
 
 namespace WSVenta.Controllers
 {
@@ -11,5 +14,100 @@ namespace WSVenta.Controllers
     [ApiController]
     public class ClienteController : ControllerBase
     {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            Respuesta oRespuesta = new Respuesta();
+            try
+            {
+
+                using (VentaRealContext db = new VentaRealContext())
+                {
+                    var lst = db.Cliente.ToList();
+                    oRespuesta.Exito = 1;
+                    oRespuesta.Data = lst;
+                }
+            }
+            catch (Exception ex)
+            {
+                oRespuesta.Mensaje = ex.Message;
+            }
+
+            return Ok(oRespuesta);
+
+        }
+
+        [HttpPost]
+        public IActionResult Add(ClienteRequest oModel) {
+
+            Respuesta oRespuesta = new Respuesta();
+            try
+            {
+                using (VentaRealContext db = new VentaRealContext()) {
+                    Cliente oCliente = new Cliente();
+                    oCliente.Nombre = oModel.Nombre;
+                    db.Cliente.Add(oCliente);
+                    db.SaveChanges();
+                    oRespuesta.Exito = 1;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                oRespuesta.Mensaje = ex.Message;
+            }
+            return Ok(oRespuesta);
+
+        }
+
+        [HttpPut]
+        public IActionResult Edit(ClienteRequest oModel)
+        {
+
+            Respuesta oRespuesta = new Respuesta();
+            try
+            {
+                using (VentaRealContext db = new VentaRealContext())
+                {
+                    Cliente oCliente = db.Cliente.Find(oModel.Id);
+                    oCliente.Nombre = oModel.Nombre;
+                    db.Entry(oCliente).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                    db.SaveChanges();
+                    oRespuesta.Exito = 1;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                oRespuesta.Mensaje = ex.Message;
+            }
+            return Ok(oRespuesta);
+
+        }
+
+        [HttpDelete("{Id}")]
+        public IActionResult Delete(int Id)
+        {
+
+            Respuesta oRespuesta = new Respuesta();
+            try
+            {
+                using (VentaRealContext db = new VentaRealContext())
+                {
+                    Cliente oCliente = db.Cliente.Find(Id);
+                    db.Remove(oCliente);
+                    db.SaveChanges();
+                    oRespuesta.Exito = 1;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                oRespuesta.Mensaje = ex.Message;
+            }
+            return Ok(oRespuesta);
+
+        }
+
     }
 }
